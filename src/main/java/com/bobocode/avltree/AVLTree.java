a2d9196b21370d428952d5f154950cde45f672a7
mfilimonov <m.filimonov@finhike.com>
2023-10-01 20:17:31 +0300

    simplified insertion method due to returning subtree root we not require do specific logic with root inside overloaded method, ad also we can use new logic with returning new Node and just cal insert for left and right in case when they null

--- src/main/java/com/bobocode/avltree/AVLTree.java
+++ src/main/java/com/bobocode/avltree/AVLTree.java
@@ -11,24 +11,16 @@ public class AVLTree {
 
   private Node insert(Node node, int value) {
     var newNode = new Node(value);
-    if (this.root == null) {
-      this.root = new Node(value);
-      node = this.root;
-    } else if (newNode.value < node.value) {
-      if (node.left == null) {
-        node.left = newNode;
-      } else {
-        node.left = insert(node.left, value);
-      }
-    } else if (newNode.value > node.value) {
-      if (node.right == null) {
-        node.right = newNode;
-      } else {
-        node.right = insert(node.right, value);
-      }
+    if (node == null) {
+      return new Node(value);
     }
-    calculateHeight(node);
+    if (newNode.value < node.value) {
+        node.left = insert(node.left, value);
+    } else if (newNode.value > node.value) {
+        node.right = insert(node.right, value);
+    }
 
+    calculateHeight(node);
     var balance = calculateBalance(node);
 
     // LL rotation
